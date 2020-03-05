ab43daccab673d636fac368b2a1395f95c60055f
GmoneyAlan <alan200@outlook.com>
2020-03-05 12:18:39 -0700

    Finished Test Cases, Starting on documentation

--- java/src/SortOfSortTester.java
+++ java/src/SortOfSortTester.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SortOfSortTester {
-
+    //Test 1 is to see if the array is working with a simple length 6 array
     @Test
     public void sortOfSort1(){
         SortOfSort sos = new SortOfSort();
@@ -11,7 +11,8 @@ public class SortOfSortTester {
         sos.sortOfSort(a);
         assertArrayEquals(m, a);
     }
-
+    //This test case is to see if the array will stay sorted the way our lab requires it
+    //or if it'll mess up the sortOfSorted
     @Test
     public void sortOfSort2(){
         SortOfSort sos = new SortOfSort();
@@ -20,23 +21,32 @@ public class SortOfSortTester {
         sos.sortOfSort(alan);
         assertArrayEquals(alan2, alan);
     }
-
+    //
     @Test
     public void sortOfSort3(){
         SortOfSort sos = new SortOfSort();
-
+        int[] alan = {1};
+        int[] alan2 = {1};
+        sos.sortOfSort(alan);
+        assertArrayEquals(alan2, alan);
     }
 
     @Test
     public void sortOfSort4(){
         SortOfSort sos = new SortOfSort();
-
+        int[] alan = {1,2,3,4,5,6,7};
+        int[] alan2 = {5,4,1,2,3,6,7};
+        sos.sortOfSort(alan);
+        assertArrayEquals(alan2, alan);
     }
 
     @Test
     public void sortOfSort5(){
         SortOfSort sos = new SortOfSort();
-
+        int[] alan = {-1,-2,-7,-3,10,30, 5};
+        int[] alan2 = {5,-1,-7,-3,-2,10,30};
+        sos.sortOfSort(alan);
+        assertArrayEquals(alan2, alan);
     }
 
 }
